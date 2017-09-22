b16af145aad15f3056f66489763dbe9e2c9373bc
Mary Yen <mymaryyn@gmail.com>
2017-09-22 14:40:15 -0400

    feat: added state to component to display variable num cards

--- client/components/main.js
+++ client/components/main.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 import { connect } from 'react-redux'
 import { withRouter, Link } from 'react-router-dom'
@@ -10,49 +10,73 @@ import { logout } from '../store'
  *  else common to our entire app. The 'picture' inside the frame is the space
  *  rendered out by the component's `children`.
  */
-const Main = (props) => {
-  const { children, handleClick, isLoggedIn } = props
+class Main extends Component {
 
-  const divStyle = {
-    marginLeft: "50px"
+  constructor(props) {
+    super(props);
+    this.state = {
+      textCards: [{text: "one"}, {text: "two"}, {text: "three"}]
+    }
+    console.log("MOUNTED", this.state.textCards)
   }
 
-  const textAreaStyle = {
-    border: "none",
-    background: "transparent",
-    outline: 0,
-
+  componentDidMount() {
   }
 
-  return (
-    <div>
-      <div id="diagramContainer" className="drag-drop-canvas">
-        <div id="item_left" className="item">
-           <textarea rows="5" id="comment" className="textCard" ></textarea> 
+  render() {
+    const { children, handleClick, isLoggedIn } = this.props
+
+    const divStyle = {
+      marginLeft: "50px"
+    }
+
+    const textAreaStyle = {
+      border: "none",
+      background: "transparent",
+      outline: 0,
+
+    }
+
+    return (
+      <div>
+        {/* Create card Button */}
+        {/* Create cards on state */}
+        <div id="diagramContainer" className="drag-drop-canvas">
+          {this.state &&  console.log("DIAGRAM", this.state)}
+          {
+            this.state && this.state.textCards.map((textCard) => (
+              <div id="item_left" key={textCard.text} className="item">
+                <textarea rows="5" id="comment" className="textCard" >{textCard.text}</textarea>
+              </div>
+            ))
+          }
+          {/* <div id="item_left" className="item">
+            <textarea rows="5" id="comment" className="textCard" ></textarea>
+          </div>
+          <div id="item_right" className="item" style={divStyle}></div> */}
         </div>
-        <div id="item_right" className="item" style={divStyle}></div>
-      </div>
 
-      <h1>BOILERMAKER</h1>
-      <nav>
-        {
-          isLoggedIn
-            ? <div>
-              {/* The navbar will show these links after you log in */}
-              <Link to='/home'>Home</Link>
-              <a href='#' onClick={handleClick}>Logout</a>
-            </div>
-            : <div>
-              {/* The navbar will show these links before you log in */}
-              <Link to='/login'>Login</Link>
-              <Link to='/signup'>Sign Up</Link>
-            </div>
-        }
-      </nav>
-      <hr />
-      {children}
-    </div>
-  )
+        <h1>BOILERMAKER</h1>
+        <nav>
+          {
+            isLoggedIn
+              ? <div>
+                {/* The navbar will show these links after you log in */}
+                <Link to='/home'>Home</Link>
+                <a href='#' onClick={handleClick}>Logout</a>
+              </div>
+              : <div>
+                {/* The navbar will show these links before you log in */}
+                <Link to='/login'>Login</Link>
+                <Link to='/signup'>Sign Up</Link>
+              </div>
+          }
+        </nav>
+        <hr />
+        {children}
+      </div>
+    )
+  }
 }
 
 /**
