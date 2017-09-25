5af7cefc3e670c6aa4a62dff1b35ffa9aee029ca
Mary Yen <mymaryyn@gmail.com>
2017-09-25 09:43:42 -0400

    cleanup

--- client/components/main.js
+++ client/components/main.js
@@ -29,52 +29,66 @@ class Main extends Component {
     this.ref.on('value', (snapshot) => { //async!!
       this.setState({ textCards: snapshot.val().textCards }) //async !!!
       console.log("MY STATE", this.state)
-      this.connect();
+      this.connect(this.state.textCards);
     })
   }
 
   AddCard = (evt) => {
     evt.preventDefault();
-
     const newTextCards = [...this.state.textCards, { text: "", id: this.state.textCards.length }];
     this.setState({ textCards: newTextCards })
     this.ref.update({ "textCards": newTextCards })
-    // this.connect();
   }
 
-  // handleSubmit (evt) {
-  //   evt.preventDefault();
-
-  //   const { name, newMessageEntry } = this.state;
-  //   const content = newMessageEntry;
-  //   const { channelId } = this.props;
-
-  //   store.dispatch(postMessage({ name, content, channelId }));
-  //   store.dispatch(writeMessage(''));
-  // }
-
-
-
-  connect() {
+  connect = (cards) => {
     console.log("IM CONNECTING")
     // create own component?
     jsPlumb.ready(function () {
-      
-      jsPlumb.connect({
-        source: "item_left",
-        target: "item_right",
-        endpoint: "Rectangle"
-      });
+      //TODO: not recognized as func?
+      // jsPlumb.setContainer("diagramContainer");
+
+      //create all connectors
+      var common = {
+        isSource: true,
+        isTarget: true,
+        connector: ["Straight"]
+      };
+
+      cards.forEach((card) => {
+        jsPlumb.addEndpoint(`item_${card.id}`, {
+          anchor: ["Right"]
+        }, common);
+        jsPlumb.addEndpoint(`item_${card.id}`, {
+          anchor: "Left"
+        }, common);
+        jsPlumb.addEndpoint(`item_${card.id}`, {
+          anchor: "Top"
+        }, common);
+        jsPlumb.addEndpoint(`item_${card.id}`, {
+          anchor: "Bottom"
+        }, common);
+
+      })
+
+      //connect related cards loaded from store
+      // jsPlumb.connect({
+      //   source: "item_0",
+      //   target: "item_right",
+      //   endpoint: "Rectangle"
+      // });
+
       jsPlumb.draggable(jsPlumb.getSelector(".drag-drop-canvas .item"));
     });
   }
 
+  //debug: adding space to make cards easier to read
+  //returns obj with marginLeft
+  addSpacingStyle = (ind) => {
+    return { marginLeft: `${ind * 100}px` }
+  }
+
   render() {
     const { children, handleClick, isLoggedIn } = this.props
-
-    const divStyle = {
-      marginLeft: "50px"
-    }
     console.log("RENDERING");
     return (
       <div>
@@ -82,19 +96,7 @@ class Main extends Component {
 
         <form id="new-message-form" onSubmit={this.AddCard}>
           <div className="input-group input-group-lg">
-            <input
-              className="form-control"
-              type="text"
-              name="content"
-              value={this.state.newMessageEntry}
-              onChange={this.handleChange}
-              placeholder="Say something nice..."
-            />
-            <span className="input-group-btn">
               <button type="submit" className="btn btn-default">Add Card</button>
-              {/* <button onClick={this.AddCard} type="button" className="btn">Add Card</button> */}
-              {/* <button className="btn btn-default" type="submit">Chat!</button> */}
-            </span>
           </div>
         </form>
 
